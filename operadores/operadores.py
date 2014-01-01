ec758e7fbdcd6d44e00855d40417fdec695aa1a5
JesusGavilan <jegavilgar@gmail.com>
2014-01-01 15:57:59 +0100

    Agregado exemplo simple de operadores

---
+++ operadores/operadores.py
@@ -0,0 +1,15 @@
+suma = 50 + 20
+resta = 40 - 20
+multiplicacion = 34 * 30
+potencia = 2 ** 6
+division = 7.5 / 2
+divisionEntera = 7.5 // 2
+modulo = 7%2
+
+print "Resultado suma: " + str(suma)
+print "Resultado resta: " + str(resta)
+print "Resultado multiplicacion: " + str(multiplicacion)
+print "Resultado exponente: " + str(potencia)
+print "Resultado division: " + str(division)
+print "Resultado divisioEntera: " + str(divisionEntera)
+print "Resultado modulo: %i ", modulo
